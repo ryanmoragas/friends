5195b3267b77427c4d312b7fe7765294791da53e
Ryan Moragas <ryanmoragas@gmail.com>
2020-06-23 14:51:03 -0500

    finished associations

--- lib/friends/movie.ex
+++ lib/friends/movie.ex
@@ -5,5 +5,7 @@ defmodule Friends.Movie do
     field :title, :string
     field :tagline, :string
     has_many :characters, Friends.Character
+    has_one :distributor, Friends.Distributor
+    many_to_many :actors, Friends.Actor, join_through: "movies_actors"
   end
 end
